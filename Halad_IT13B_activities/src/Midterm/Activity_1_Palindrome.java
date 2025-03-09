5c3e4b680e78d585eb3137f4d6298a4a858b1fc6
Hamed Ansari Halad <haladhamed@gmail.com>
2025-03-09 08:47:19 +0845

    first midterm act PALINDROME
    PALINDROME

--- Halad_IT13B_activities/src/Midterm/Activity_1_Palindrome.java
+++ Halad_IT13B_activities/src/Midterm/Activity_1_Palindrome.java
@@ -1,10 +1,28 @@
 package Midterm;
 
+import java.util.Scanner;
 
 public class Activity_1_Palindrome {
 
     public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
 
-      
+        System.out.println("Enter your text");
+        String string = a.next();
+
+        String org_str = string;
+        int length = string.length();
+
+        String reverse = "";
+
+        for (int i = length - 1; i >= 0; i--) {
+            reverse = reverse + string.charAt(i);
+        }
+        if (org_str.equals(reverse)) {
+            System.out.println(" ITS A PALINDROME ");
+        } else {
+            System.out.println(" ITS NOT A PALINDROME ");
+        }
+
     }
 }
