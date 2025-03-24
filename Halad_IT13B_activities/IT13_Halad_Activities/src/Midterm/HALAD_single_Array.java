8a8bbf2edf40df77380b5812bb06fe120c929580
Ronabaldoza <>
2025-03-24 10:49:52 +0800

    added singe and double array

---
+++ Halad_IT13B_activities/IT13_Halad_Activities/src/Midterm/HALAD_single_Array.java
@@ -0,0 +1,13 @@
+package Midterm;
+
+public class HALAD_single_Array {
+
+    public static void main(String[] args) {
+
+        String[] array = {"A", "B", "C", "D", "E", "F", "G"};
+        for (String letter : array) {
+            System.out.print(letter + ", ");
+
+        }
+
+    }}
